e178a8615a95fbd1134d5cb3e23c71d783da9beb
Matt Tierney <tierney@cs.nyu.edu>
2013-04-18 09:02:55 -0400

    Using simplified main.

--- src/main.cc
+++ src/main.cc
@@ -7,30 +7,21 @@
 #include "base64.h"
 
 int main(int argc, char** argv) {
+  cryptagram::BlockCipher bc;
+  std::string output;
+
   if (argc != 3) {
     std::cerr << "Expected usage:\n  " << argv[0] << " "
               << "\"Message\" \"Password\"" << std::endl;
     return 1;
   }
 
-  scoped_ptr<cryptagram::BlockCipher> bc;
-  for (int i = 0; i < 10; i++) {
-    bc.reset(new cryptagram::BlockCipher());
+  bc.Encrypt(argv[1], argv[2], &output);
+  std::cout << base64_encode(output) << std::endl;
 
-    std::string output;
+  std::string final;
+  bc.Decrypt(output, argv[2], &final);
 
-    bc->Encrypt(argv[1], argv[2], &output);
-    std::cout << base64_encode(output) << std::endl;
-
-    bc.reset(new cryptagram::BlockCipher());
-    std::string final;
-    bc->Decrypt(output, argv[2], &final);
-
-    std::cout << final << std::endl;
-    if (final != argv[1]) {
-      bc->PrintKey();
-      bc->PrintSalt();
-    }
-  }
+  std::cout << final << std::endl;
   return 0;
 }
